d49d4c4f0163bae0ad540e9ea67d522547856bda
Ruhil Jaiswal <ruhiljaiswal@gmail.com>
2017-04-11 13:43:03 +0530

    dev-env3 structure

---
+++ .dev_env/flags/gen-docs.sh
@@ -0,0 +1,8 @@
+dev-script run_container --name sphinx -it \
+    -v $ROOT_PATH:$ROOT_PATH_CONTAINER \
+    -v $COMMON_UTIL:$COMMON_UTIL_CONTAINER \
+    -v $CODE_MANAGER:$CODE_MANAGER_CONTAINER \
+    -v $REPO_PATH:$REPO_PATH_CONTAINER \
+    -v $SPHINX_PATH:$SPHINX_PATH_CONTAINER \
+    --img=$IMG $SPHINX_PATH_CONTAINER/documentation.sh 
+
