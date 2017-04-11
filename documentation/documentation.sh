8066a65346d5667a7ef92ebc61be2ab7149594ef
Ruhil Jaiswal <ruhiljaiswal@gmail.com>
2017-04-11 13:45:19 +0530

    automation

--- documentation/documentation.sh
+++ documentation/documentation.sh
@@ -1,31 +1,49 @@
+#!/usr/bin/env bash
+
 dir=`pwd`/docs2/
 projectdir=/0x4bdn
-if test "$(ls -A  $dir)"; then
-	echo "not empty"
-else
-	echo "empty"
-	sphinx-quickstart $dir \
-		--quiet \
-		--project="Code Inegration System" \
-		--author="0x4bdn" \
-		-v "1.0" \
-		--release="1.0 beta" \
-		--language="en" \
-		--suffix=".rst"  \
-		--ext-doctest "y" \
-		--ext-intersphinx "y" \
-		--ext-autodoc "y" \
-		--ext-todo "y" \
-		--ext-coverage "y" \
-		--makefile "y"
-	
-fi
+
+rm -r $dir
+mkdir $dir
+
 echo "Generating program list"
 find $projectdir -name "[a-z]*.py" | awk -F "/" '{print $NF}' >filelist.txt
-echo "Generating path list"
-# find $projectdir -name "[a-z]*.py" | awk -F "/" '{OFS = "/"}{$(NF--)=""; }{print}' >dirlist.txt
-echo "removing old ReStructuredText files"
-rm -r $dir/*.rst
+
+echo " generating the docs "
+find $projectdir -name "[a-z]*.py" | awk -F "/" '{OFS = "/"}{$(NF--)=""; }{print}' >dirlist.txt
+
+sphinx-quickstart $dir \
+--quiet \
+--project="Code Inegration System" \
+--author="0x4bdn" \
+-v "1.0" \
+--release="1.0 beta" \
+--language="en" \
+--suffix=".rst"  \
+--ext-doctest "y" \
+--ext-intersphinx "y" \
+--ext-autodoc "y" \
+--ext-todo "y" \
+--ext-coverage "y" \
+--makefile "y"
+
+_content = '
+import os
+from os.path import split, abspath, join
+from sys import path
+path.insert(0, abspath('.'))
+
+base_path = split(split(abspath(os.getcwd()))[0])[0]
+with open(join(base_path, "documentation", "dirlist.txt"), "r") as fp:
+dir_list=fp.read().splitlines()
+
+dir_set=set(dir_list)
+print(dir_set)
+for dir_path in dir_set:
+path.append(dir_path)
+'
+echo "${_content}" >> ./docs2/conf.py
+
 echo "Generating code.rst"
 python3 documentation.py
 make -C $dir clean
