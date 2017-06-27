e471f0b3221422d5769a13817cb4ba11c05f412f
Ruhil Jaiswal <ruhiljaiswal@gmail.com>
2017-06-27 11:10:10 +0530

    support files

---
+++ sample/samplefn/isgreaterthan10.py
@@ -0,0 +1,12 @@
+"""
+Basic filter
+"""
+
+TAGS = ["math", 'is greater', "filter", "is greater than 10"]
+
+def isgreaterthan10(num: int) -> bool:
+    """
+    For a given number identify if it is grater than 10 or not
+    Type: Filter
+    """
+    return num > 10
